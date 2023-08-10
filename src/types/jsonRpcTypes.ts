16c78b9051fb38768866c8cf7a080911d05ca6e8
Fenton Taylor <fentontaylor@gmail.com>
2023-08-10 09:41:28 -0600

    Add response types to jsonRpcTypes

--- src/types/jsonRpcTypes.ts
+++ src/types/jsonRpcTypes.ts
@@ -1,13 +1,27 @@
-/* SESSION REQUEST PARAMS */
-export type HederaSignAndExecuteTransactionParams = {
+import { TransactionReceipt, TransactionResponse } from '@hashgraph/sdk'
+
+type TransactionParams = {
   transaction: {
     type: string
     bytes: string
   }
 }
 
-export type HederaSignAndReturnTransactionParams = HederaSignAndExecuteTransactionParams
+/** hedera_signAndExecuteTransaction */
+export type HederaSignAndExecuteTransactionParams = TransactionParams
+export type HederaSignAndExecuteTransactionResponse = {
+  response: TransactionResponse
+  receipt: TransactionReceipt
+}
 
+/** hedera_signAndReturnTransaction */
+export type HederaSignAndReturnTransactionParams = TransactionParams
+export type HederaSignAndReturnTransactionResponse = TransactionParams
+
+/** hedera_signMessage */
 export type HederaSignMessageParams = {
   message: string
+}
+export type HederaSignMessageResponse = {
+  signature: string
 }
