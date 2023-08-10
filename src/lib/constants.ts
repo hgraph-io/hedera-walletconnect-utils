e525148a3b8e080aab572a1f1762a345f45e4b3b
Fenton Taylor <fentontaylor@gmail.com>
2023-08-10 09:06:13 -0600

    Add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const HEDERA_CRYPTO_NAME = 'Hedera HBAR'
+export const HEDERA_CRYPTO_SYMBOL = 'HBAR'
+export const HEDERA_HBAR_SIGN = 'ℏ'
+/**
+ * BIP-0044 defines a logical hierarchy for deterministic wallets.
+ * Level 2 of the hierarchy describes a coin type in use.
+ * {@link https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki}
+ * {@link https://github.com/satoshilabs/slips/blob/master/slip-0044.md}
+ */
+export const HEDERA_SLIP0044_COIN_TYPE = 3030
+export const HEDERA_BIP0044_CONSTANT = 0x80000bd6
