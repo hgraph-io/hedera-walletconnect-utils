639e392e562b6fa7cf905557789fb946f7b500e9
BeeJee <BeeJeeNinja@gmail.com>
2023-11-20 12:22:31 +0200

    fix dev configs

--- scripts/examples/dev.mjs
+++ scripts/examples/dev.mjs
@@ -8,14 +8,19 @@ let ctx8081 = await esbuild.context(config)
  * watches for file changes and serves most recent files
  */
 async function main() {
-  ctx8080.serve({
+  const server1 = await ctx8080.serve({
     servedir: 'dist/examples/typescript',
+    host: 'localhost',
     port: 8080,
   })
-  ctx8081.serve({
+  const server2 = await ctx8081.serve({
     servedir: 'dist/examples/typescript',
+    host: 'localhost',
     port: 8081,
   })
+
+  console.log(`Server 1 is up ${server1.host}:${server1.port}`);
+  console.log(`Server 2 is up ${server2.host}:${server2.port}`);
 }
 
 await main()
